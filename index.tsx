e618ea888a2de891cdb87bb31fe88597cdd078e0
cjativa <cjativa@barnesfoundation.org>
2020-05-01 20:53:23 -0400

    1 file modified

--- index.tsx
+++ index.tsx
@@ -3,6 +3,12 @@ import { render } from 'react-dom';
 import './styles.scss';
 
 const Application = () => {
+
+  const config = {
+    name: `Me`,
+    computer: `Ubuntu`
+  };
+
   return (
     <div className="application">
     </div>
